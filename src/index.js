420a1f6ffec5a04f43eeb434175f3b0eba3ad48a
Sebastian <sebasdm5@gmail.com>
2023-06-11 00:57:54 +1000

    tests

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import MyTable from "./MyTable";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
 
