4005109018840dea0fd4ab80137eef63c49a1a81
Sebastian <sebasdm5@gmail.com>
2023-06-11 00:53:19 +1000

    testsetsetest

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import MyTable from "./MyTable";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MyTable/>
+    <App/>
   </React.StrictMode>
 );
 
