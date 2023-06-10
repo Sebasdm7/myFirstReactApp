dbf2fa153d0d17fa51e9839c8bd33d4c87c8503f
Sebastian <sebasdm5@gmail.com>
2023-06-11 00:44:41 +1000

    trest dsf

--- src/index.js
+++ src/index.js
@@ -12,9 +12,7 @@ import MyTable from "./MyTable";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App/>
-    </HashRouter>
+    <MyTable/>
   </React.StrictMode>
 );
 
